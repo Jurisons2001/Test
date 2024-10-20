73d6423026027e2f63a575a56affd0b518907f8e
Jurisons2001 <161339697+Jurisons2001@users.noreply.github.com>
2024-10-20 13:02:55 +0300

    Fails ir atjaunināts

--- jauns_fails.py
+++ jauns_fails.py
@@ -1,13 +1,8 @@
 Tiek pievienots viens jauns commits
-++
 Tiek pievienots otrais commits!
-
 X=5;
-
 šis ir jauns commit
-
 y=3;
-
 Treniņš norit labi
-
 Tiek veikti uzlabojumi
+Pēdējais commits failam
